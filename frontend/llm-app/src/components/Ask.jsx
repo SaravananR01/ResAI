8cc22b428173d356a56c2c19ce7e1e5e649e4707
Saravanan <saravananr0104@gmail.com>
2025-07-13 19:48:59 +0530

    query through frontend done

--- frontend/llm-app/src/components/Ask.jsx
+++ frontend/llm-app/src/components/Ask.jsx
@@ -1,17 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
+import { FaPaperclip } from "react-icons/fa";
+import api from '../services/api';
+
 
 function Ask({ onSend }) {
   const [query, setQuery] = useState('');
 
   const handleChange = (e) => setQuery(e.target.value);
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (query.trim()) {
-      onSend(query);
+      const response= await api.post('/ask',{question:query});
       setQuery('');
     }
   };
+  const [selectedFileName,setSelectedFileName]=useState('');
+  const [selectedFile,setSelectedFile]=useState(null);
+  const fileInputRef=useRef(null);
+
+  const handleFileChange=(e)=>{
+      setSelectedFile(e.target.files[0]);
+      setSelectedFileName(e.target.files[0]?.name||'');
+  }
+
+  const handleUpload=async()=>{
+      if(!selectedFile){
+          alert("Please Select a File");
+          return;
+      }
+      const formData = new FormData();
+      formData.append('file',selectedFile);
+
+      try{
+          const response=await api.post('/file-upload',formData,{
+              headers: {
+                  'ContentType':'multipart/form-data',
+              },
+              withCredentials:true
+          });
+          console.log('Upload success: ',response.data);
+          alert("File Uploaded successfully");
+      }catch(error){
+          console.log("Upload Failed");
+          alert("File Upload Failed");
+
+      }
+  }
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') handleSubmit();
@@ -19,6 +54,19 @@ function Ask({ onSend }) {
 
   return (
     <div className="w-[90%] max-w-2xl bg-white text-gray-900 p-4 rounded-2xl border border-gray-300 hover:border-indigo-500 shadow-sm flex items-center">
+      <input
+          type='file'
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className='hidden'
+          name='file'
+        />
+        <button
+          onClick={() => fileInputRef.current.click()}
+          className='bg-white px-1 py-1 mr-1 rounded-full shadow-md transition font-semibold w-48 text-left hover:cursor-pointer'
+        >
+          <FaPaperclip className='inline-block'/> {selectedFileName || 'Choose a file'}
+        </button>
       <input
         type="text"
         onChange={handleChange}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { askGemini } from './agent.js';
+import multer from 'multer';
+//import PdfParse from 'pdf-parse';
 
 const port=8080 || process.env.PORT;
 
@@ -15,7 +17,7 @@ app.listen(port,()=>{
     console.log(`Server Listening on PORT : ${port}`);
 });
 
-
+// ask endpoint
 app.post('/ask', async (req,res)=>{
     console.log("call works");
     const question = req.body.question;
@@ -29,3 +31,14 @@ app.post('/ask', async (req,res)=>{
         res.status(500).json({error:"something went wrong"});
     }
 });
+
+
+//upload
+const upload=multer({dest:'uploads/'});
+
+// app.post('/upload',upload.single('file'),async (req,res)=>{
+//     const file=req.file;
+//     const content=await PdfParse(file.path);
+//     console.log(content);
+//     res.json({content});
+// })
